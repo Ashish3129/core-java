961ec4dd0e0a7efa85c58a945f3c7e930119164a
Ashis <Ashis@10.0.0.120>
2023-10-03 16:45:11 +0530

    Fourth Commit

---
+++ project1/src/com/tns/singleinheritance/SingleDemo.java
@@ -0,0 +1,11 @@
+package com.tns.singleinheritance;
+
+public class SingleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student st=new Student("Ashish",724739,"Penugonda",79952);
+		System.out.println(st);
+	}
+
+}
